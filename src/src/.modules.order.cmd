fec974859d6d8eaa05e7ee6580ad50a3b29bae66
Devsstudent <devsstudent777@gmail.com>
2024-11-07 22:03:44 +0100

    clean

--- src/src/.modules.order.cmd
+++
@@ -1,1 +0,0 @@
-savedcmd_/home/hanako/base/src/src/modules.order := {   echo /home/hanako/base/src/src/rootkit.o; :; } > /home/hanako/base/src/src/modules.order
--- src/src/.rootkit.mod.cmd
+++
@@ -1,1 +0,0 @@
-savedcmd_/home/hanako/base/src/src/rootkit.mod := printf '%s\n'   rootkit.o | awk '!x[$$0]++ { print("/home/hanako/base/src/src/"$$0) }' > /home/hanako/base/src/src/rootkit.mod
