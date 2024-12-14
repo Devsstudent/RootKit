ad8be5716aea2ee8aa256c9102dfeea98846510d
hanako <devsstudent777@gmail.com>
2024-12-14 17:31:16 +0100

    add back missing interface network qemu

---
+++ src/.Module.symvers.cmd
@@ -0,0 +1,1 @@
+savedcmd_/home/hanako/RootKit/src/Module.symvers :=  scripts/mod/modpost -M -m -a      -o /home/hanako/RootKit/src/Module.symvers -T /home/hanako/RootKit/src/modules.order -i Module.symvers -e 
